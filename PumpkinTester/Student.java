1b0da3c16e1dc1448f32963d4f973c8696c6bf72
Tal Dickman <tdickman@WS-ACADBUS.EDU>
2020-01-08 10:20:35 -0800

    commit

---
+++ PumpkinTester/Student.java
@@ -0,0 +1,21 @@
+
+/**
+ * An example of using encapsulation in a class
+ *
+ * @author Tal Dickman
+ * @version 1/18/20
+ */
+
+public class Student
+{
+    private String name;
+    private int age;
+    public Student(){
+        
+    }
+    public Student(String name, int age){
+        this.name = name;
+        this.age = age;
+    }
+}
+
